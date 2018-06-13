707ab71a63795f51f1a244bcd67ebe780a5d4704
James Wilcox <james@wilcox.net.nz>
2018-06-13 16:20:48 +1200

    Version 1.1 updates

--- BusNotifier.ps1
+++ BusNotifier.ps1
@@ -1,5 +1,5 @@
-﻿Param(
-  [int]$StopId = 5000, 
+Param(
+  [string]$StopId = 5000, 
   [string[]]$Routes,
   [switch]$Debug,
   [int]$TimeToBusStop = 5
@@ -9,6 +9,36 @@ $Timeframe = 3600
 
 function Show-Notification ($title, $body)
 {
+    $app = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
+
+    $Template = [Windows.UI.Notifications.ToastTemplateType]::ToastImageAndText04
+
+    #Gets the Template XML so we can manipulate the values
+    [xml]$ToastTemplate = ([Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent($Template).GetXml())
+
+    [xml]$ToastTemplate = @"
+    <toast launch="app-defined-string">
+      <visual>
+        <binding template="ToastGeneric">
+          <text>$title</text>
+          <text>$body</text>
+        </binding>
+      </visual>
+      <actions>
+        <action activationType="background" content="Dismiss" arguments="later"/>
+      </actions>
+    </toast>
+"@
+
+    $ToastXml = New-Object -TypeName Windows.Data.Xml.Dom.XmlDocument
+    $ToastXml.LoadXml($ToastTemplate.OuterXml) | Out-Null
+
+    $notify = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($app)
+
+    $notify.Show($ToastXml) | Out-Null
+
+    <#
     [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
     # Toasts templates: https://msdn.microsoft.com/en-us/library/windows/apps/hh761494.aspx
     $template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText04)
@@ -30,6 +60,7 @@ function Show-Notification ($title, $body)
     # Create the toats and show the toast. Make sure to include the AppId
     $notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($toast.Tag)
     $notifier.Show($toast);
+    #>
 }
 
 $Bytes = [BitConverter]::GetBytes(0x1F525)
@@ -40,9 +71,43 @@ $WarningEmoji = [System.Text.Encoding]::UTF32.GetString($Bytes)
 
 $index = 1
 $ServiceCount = 0
+$Mode = 'Bus'
+
+#Train lines must be specified in uppercase
+if ($StopId -notmatch '^\d+$')
+{
+    $StopId = $StopId.ToUpper()
+    $Mode = 'Train'
+}
+
+#Check parameters are valid before proceeding
+try
+{
+    Invoke-WebRequest "https://www.metlink.org.nz/api/v1/StopDepartures/$StopId" | Out-Null
+}
+catch
+{
+    Write-Error "Exception calling Metlink API. Check your parameters are valid."
+    break
+}
 
 $StopName = ((Invoke-WebRequest "https://www.metlink.org.nz/api/v1/StopDepartures/$StopId") | ConvertFrom-Json).Stop.Name
+if ($Routes -eq $null)
+{
+    $RouteSummary = "ALL"
+}
+
+else
+{
+    $RouteSummary = $Routes
+}
 Write-Host $StopName
+Show-Notification -title "Metlink Bus Notifier" -body "Getting departure results for route(s) $RouteSummary in next 20 services..."
+if ($Debug -ne $true)
+{
+    Start-Sleep -Seconds 7
+}
+
 
 do {
     $ApiResponse = ''
@@ -107,7 +172,14 @@ do {
             }
         
             if ($RealTime -eq $true) {
-                $LogEntry = "$Route to $Destination - $MinutesAway minutes away"
+                if ($Delta -eq 0) {
+                    $LogEntry = "$Route to $Destination - $MinutesAway minutes away ($Status)"
+                }
+
+                else { 
+                    $LogEntry = "$Route to $Destination - $MinutesAway minutes away ($delta minutes $Status)" 
+                }
+                
 
                 if ($LeaveNow -eq $true) { $LogEntry += " (leave now)" }
 
@@ -121,7 +193,7 @@ do {
             
             
             if ($Debug -eq $false) {
-                Show-Notification -title ("$WarningEmoji Bus Alert: " + $ApiResponse.Stop.Name) -body $Body
+                Show-Notification -title ("$WarningEmoji $Mode Notifier Alert: " + $ApiResponse.Stop.Name) -body $Body
                 Start-Sleep -Seconds 7
             }
             if ($Debug -eq $true) { 
